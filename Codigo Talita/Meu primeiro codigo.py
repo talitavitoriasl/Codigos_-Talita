ecfef487c04d20eac607babe9c8bfd330520086c
Talita Vitoria <talitavitoriadasilvaxr25@gmail.com>
2025-08-28 15:49:30 -0300

    Subindo meus códigos

---
+++ Codigo Talita/Meu primeiro codigo.py
@@ -0,0 +1,7 @@
+nome = input ("Qual o seu nome?")
+print(nome)
+nome = input ("Qual e sua idade?")
+print(idade)
+nome = input ("Qual e a sua coSr favorita?)
+print (comida)
+]
---
+++ Codigo Talita/Meu segundo programa.py
@@ -0,0 +1,11 @@
+#print("Talita" *80000)
+#exemplos de variados variado do tipo texto
+fruta = "jabuticaba"
+print(type (fruta))
+### EXERCICIOS 1
+nome = input("Digite seu nome")
+print(nome)
+nota = float(input("Digite a sua nota")
+print(nota)
+idade = int(input("Digite sua idade")
+            
